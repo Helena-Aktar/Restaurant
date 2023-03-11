c4bc50ca8ef849e6a7c7c2b02ad7f097140726d2
Helena Akter Reya <helena.aktar@ndessl.local>
2023-03-11 13:37:38 +0600

    base class problem solved

---
+++ js/menuPage.js
@@ -0,0 +1,21 @@
+// toggle menu bar
+const sidebar = document.querySelector("#side_nav_container");
+const sidebarMenuButton = document.querySelector("#sidebar-menu");
+const sidebarCrossButton = document.querySelector("#sidebar-cross-btn");
+const sidebarOutbox = document.querySelector(".sidebar-outbox");
+// console.log(sidebarCrossButton);
+const sidebarMenuItems = document.querySelector(".sidebar__menu-items");
+// menu button on click
+sidebarMenuButton.addEventListener("click", function () {
+  sidebarMenuItems.classList.toggle("active");
+  sidebar.classList.toggle("active");
+  sidebarOutbox.style.display = "block";
+
+  console.log(sidebarMenuItems.className);
+});
+// cross button on click
+sidebarCrossButton.addEventListener("click", function () {
+  sidebarMenuItems.classList.toggle("active");
+  sidebar.classList.toggle("active");
+  sidebarOutbox.style.display = "none";
+});
