6621b23af820b5e288989f63b56ed43ec5df3748
Helena Akter Reya <helena.aktar@ndessl.local>
2023-03-11 17:35:50 +0600

    vieworder section added

--- js/menuPage.js
+++ js/menuPage.js
@@ -19,3 +19,49 @@ sidebarCrossButton.addEventListener("click", function () {
   sidebar.classList.toggle("active");
   sidebarOutbox.style.display = "none";
 });
+// menu items header text
+const menuHeaderList = document.querySelectorAll(".menu-tabs-header-text");
+const weekdayLunch = document.querySelector("#weekday_lunch");
+const weekdayLunchDishes = document.querySelector("#weekday_lunch-dishes");
+const dinner = document.querySelector("#dinner");
+const dinnerDishes = document.querySelector("#dinner-dishes");
+console.log(1, weekdayLunch);
+// remove active class from menu items
+const removeActiveItem = () => {
+  menuHeaderList.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+// MENU HEADER ADD ACTIVE ITEMS
+menuHeaderList.forEach((item) => {
+  console.log(item);
+
+  item.addEventListener("click", () => {
+    removeActiveItem();
+    item.classList.add("active");
+    console.log(item.id);
+    if (item.id == "weekday_lunch") {
+      weekdayLunchDishes.style.display = "block";
+      dinnerDishes.style.display = "none";
+    } else if (item.id == "dinner") {
+      console.log(item.id);
+      weekdayLunchDishes.style.display = "none";
+      dinnerDishes.style.display = "block";
+    } else console.log("paynai");
+  });
+});
+
+// // menu header icons rotaion
+const menuHeaderIcons = document.querySelectorAll(".menu-tabs-header-icon");
+var root = document.querySelector(":root");
+var i = 0;
+console.log(menuHeaderIcons);
+function rotateIcons() {
+  setInterval(() => {
+    // menuHeaderIcons.style.transform = "rotate(45deg)";
+    root.style.setProperty("--menu_header-icons-tranform-deg", i + "deg");
+    i += 5;
+    console.log(i);
+  }, 100);
+}
+// rotateIcons();
