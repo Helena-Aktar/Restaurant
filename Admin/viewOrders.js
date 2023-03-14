fe5d08b788c91e220e310e289cb9ebb7ab06c99e
Md. Shariful Islam <shariful.islam22@ndessl.local>
2023-03-14 16:32:10 +0600

    don't know

--- Admin/viewOrders.js
+++ Admin/viewOrders.js
@@ -1,12 +1,5 @@
-// var buttonActive = document.querySelector(".Button");
-// console.log(buttonActive);
-// buttonActive.addEventListener('click',function(){
-//     if(buttonActive=="Button p-4")
-//     {
-//         buttonActive.classList.add('active');
-//     }
-// })
 
+console.log("active");
 
 function active(num)
 {
