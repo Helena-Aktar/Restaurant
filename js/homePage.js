2c656046ad5003f96c1fce3f3190f8570c387c44
Helena Akter Reya <helena.aktar@ndessl.local>
2023-03-19 18:59:30 +0600

    fetched data then created onb then pushed objects to array

--- js/homePage.js
+++ js/homePage.js
@@ -1,6 +1,41 @@
 // fetching data from json file
-fetch("./data.json")
+// fetch("./data.json")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log("json Data: " + data);
+//   });
+
+const SidebarMenuArray = [];
+
+// Fetchhing from API
+fetch("http://192.168.2.102:85/GetAllSidebarItems")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
+    // creating object from fetched data
+    data.forEach((item) => {
+      const obj = {
+        MenuID: item.sidebar_item_id,
+        MenuName: item.sidebar_item_name,
+        ParentID: item.sidebar_item_parent_id,
+        Destination: item.sidebar_item_destination,
+      };
+      // pushing objects to array
+      SidebarMenuArray.push(obj);
+    });
+    console.log(SidebarMenuArray);
+  });
+const DishItemsArray = [];
+// Fetchhing from API
+fetch("http://192.168.2.102:85/GetAllDishItems")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    // creating object from fetched data
+    data.forEach((item) => {
+      const obj = { ...item }; // spread operator (...)
+      // pushing objects to array
+      DishItemsArray.push(obj);
+    });
+    console.log(DishItemsArray);
   });
