3e780e6bb92aa26942702c9168cb350407bfbd24
Md. Shariful Islam <shariful.islam22@ndessl.local>
2023-03-30 11:54:10 +0600

    display screen work is 80% DONE

--- display/orderscript.js
+++ display/orderscript.js
@@ -9,17 +9,38 @@ function getValueFromController() {
                loop();
                function loop() {
                    var i = 0;
-                   var k = 5;
+                   var k = 10;
                    var p = 0;
+                   var parenttable =document.querySelector(".table_body");
+
+                   for(i;i<k;i++){
+                    var creatediv = document.createElement("div");
+                    creatediv.innerHTML=`
+                    <!-- {/* item ${i} */} -->
+                    <div class="table_items" id="row0">
+                        <div class="table_item" id="no${i}">123</div>
+                        <div class="table_item" id="id${i}">1234</div>
+                        <div class="table_item" id="name${i}">dfasfd fdsfdsfd</div>
+                        <div class="table_item" id="email${i}">jztrxohoe@gmail.com</div>
+                        <div class="table_item" id="phone${i}">01719785644</div>
+                        <div class="table_item" id="address${i}">
+                            H-671, H-671, Califxcsxcdxcdsfrgteorniarnia
+                        </div>
+                    </div>
+                    `
+                    parenttable.appendChild(creatediv);
+                   }
                    slide(i, k, p);
                    function slide(j, x, p) {
-                       if (p > 4) {
+
+                       if (p > orderdata.length-10) {
                            p = 0;
                        } else {
                            p = p + 1;
                        }
                        var q = 0;
                        for (j; j < x; j++) {
+                            document.getElementById("no" + q).innerHTML = "no."+j;
                            var id = document.getElementById("id" + q);
                            id.innerHTML = orderdata[j].table_number;
                            document.getElementById("name" + q).innerHTML = orderdata[j].order_total_cost;
@@ -29,54 +50,54 @@ function getValueFromController() {
 
                            // ------- Date Checker ------------
 
-                           const today = new Date();
-                           let tomorrow = new Date();
-                           tomorrow.setDate(today.getDate() + 1);
+                        //    const today = new Date();
+                        //    let tomorrow = new Date();
+                        //    tomorrow.setDate(today.getDate() + 1);
 
                            //  -------------  Date based Color Changer
 
-                           if (today.toDateString() === new Date(did).toDateString()) {
-                               console;
-                               // document.getElementById("row" + q).style.color = "white";
-                               document.getElementById("id" + q).style.background = "red";
-                               document.getElementById("name" + q).style.background = "red";
-                               document.getElementById("email" + q).style.background = "red";
-                               document.getElementById("phone" + q).style.background = "red";
-                               document.getElementById("address" + q).style.background = "red";
-                               document.getElementById("id" + q).style.color = "white";
-                               document.getElementById("name" + q).style.color = "white";
-                               document.getElementById("email" + q).style.color = "white";
-                               document.getElementById("phone" + q).style.color = "white";
-                               document.getElementById("address" + q).style.color = "white";
-                           } 
-                           else if (tomorrow.toDateString() === new Date(did).toDateString()) {
-                               // document.getElementById("row" + q).style.color = "black";
-                               document.getElementById("id" + q).style.background = "yellow";
-                               document.getElementById("name" + q).style.background = "yellow";
-                               document.getElementById("email" + q).style.background = "yellow";
-                               document.getElementById("phone" + q).style.background = "yellow";
-                               document.getElementById("address" + q).style.background = "yellow";
-                               document.getElementById("id" + q).style.color = "black";
-                               document.getElementById("name" + q).style.color = "black";
-                               document.getElementById("email" + q).style.color = "black";
-                               document.getElementById("phone" + q).style.color = "black";
-                               document.getElementById("address" + q).style.color = "black";
-                           } 
-                           else {
-                               // document.getElementById("row" + j).style.color = "white";
-                               // document.getElementById("row" + q).style.color = "red";
-                               // console.log("row" + q);
-                               document.getElementById("id" + q).style.background = "black";
-                               document.getElementById("name" + q).style.background = "black";
-                               document.getElementById("email" + q).style.background = "black";
-                               document.getElementById("phone" + q).style.background = "black";
-                               document.getElementById("address" + q).style.background = "black";
-                               document.getElementById("id" + q).style.color = "white";
-                               document.getElementById("name" + q).style.color = "white";
-                               document.getElementById("email" + q).style.color = "white";
-                               document.getElementById("phone" + q).style.color = "white";
-                               document.getElementById("address" + q).style.color = "white";
-                           }
+                        //    if (today.toDateString() === new Date(did).toDateString()) {
+                        //        console;
+                        //        // document.getElementById("row" + q).style.color = "white";
+                        //        document.getElementById("id" + q).style.background = "red";
+                        //        document.getElementById("name" + q).style.background = "red";
+                        //        document.getElementById("email" + q).style.background = "red";
+                        //        document.getElementById("phone" + q).style.background = "red";
+                        //        document.getElementById("address" + q).style.background = "red";
+                        //        document.getElementById("id" + q).style.color = "white";
+                        //        document.getElementById("name" + q).style.color = "white";
+                        //        document.getElementById("email" + q).style.color = "white";
+                        //        document.getElementById("phone" + q).style.color = "white";
+                        //        document.getElementById("address" + q).style.color = "white";
+                        //    } 
+                        //    else if (tomorrow.toDateString() === new Date(did).toDateString()) {
+                        //        // document.getElementById("row" + q).style.color = "black";
+                        //        document.getElementById("id" + q).style.background = "yellow";
+                        //        document.getElementById("name" + q).style.background = "yellow";
+                        //        document.getElementById("email" + q).style.background = "yellow";
+                        //        document.getElementById("phone" + q).style.background = "yellow";
+                        //        document.getElementById("address" + q).style.background = "yellow";
+                        //        document.getElementById("id" + q).style.color = "black";
+                        //        document.getElementById("name" + q).style.color = "black";
+                        //        document.getElementById("email" + q).style.color = "black";
+                        //        document.getElementById("phone" + q).style.color = "black";
+                        //        document.getElementById("address" + q).style.color = "black";
+                        //    } 
+                        //    else {
+                        //        // document.getElementById("row" + j).style.color = "white";
+                        //        // document.getElementById("row" + q).style.color = "red";
+                        //        // console.log("row" + q);
+                        //        document.getElementById("id" + q).style.background = "black";
+                        //        document.getElementById("name" + q).style.background = "black";
+                        //        document.getElementById("email" + q).style.background = "black";
+                        //        document.getElementById("phone" + q).style.background = "black";
+                        //        document.getElementById("address" + q).style.background = "black";
+                        //        document.getElementById("id" + q).style.color = "white";
+                        //        document.getElementById("name" + q).style.color = "white";
+                        //        document.getElementById("email" + q).style.color = "white";
+                        //        document.getElementById("phone" + q).style.color = "white";
+                        //        document.getElementById("address" + q).style.color = "white";
+                        //    }
                            q++;
                            //console.log(q);
                        }
@@ -84,15 +105,15 @@ function getValueFromController() {
                        // console.log(orderdata.length);
                        if (orderdata.length <= x) {
                            j = 0;
-                           x = 5;
+                           x = 10;
                            p = 0;
 
                            setTimeout(function () {
                                slide(j, x, p);
-                           }, 3000);
+                           }, 6000);
                        } else {
                            j = p + 0;
-                           x = p + 5;
+                           x = p + 10;
 
                            setTimeout(function () {
                                slide(j, x, p);
